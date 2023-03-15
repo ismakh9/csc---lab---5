7c5b78886d4f523b7e4ecfc354fd0969d24c2c87
gthouser <grace.houser@furman.edu>
2023-03-15 17:18:36 -0400

    updated builder files

--- src/input/builder/GeometryBuilder.java
+++ src/input/builder/GeometryBuilder.java
@@ -1,3 +1,11 @@
+/**
+ * This file builds a geometry figure and extends DefaultBuilder. 
+ *
+ * <p>Bugs: (a list of bugs and / or other problems)
+ *
+ * @author Josh Berger, Grace Houser, Khalid Ismail
+ * @date Friday, March 17th, 2023
+ */
 package input.builder;
 
 import java.util.List;
@@ -11,58 +19,38 @@ import input.components.segment.SegmentNodeDatabase;
 import input.exception.ParseException;
 import input.visitor.UnparseVisitor;
 
-public class GeometryBuilder {
+public class GeometryBuilder extends DefaultBuilder {
 
-   private PointNodeDatabase pointNodeDB;
-   private SegmentNodeDatabase segmentNodeDB;
-   private FigureNode figureNode;
-
-   public GeometryBuilder() {
-       pointNodeDB = new PointNodeDatabase();
-       segmentNodeDB = new SegmentNodeDatabase();
-   }
-
-   public void buildPointNode(String name, double x, double y) {
-       PointNode pointNode = new PointNode(name, x, y);
-       pointNodeDB.put(pointNode);
-   }
-
-   public void buildSegmentNode(String startPointName, List<String> endPointNames) throws ParseException {
-       // Retrieve the start point node from the database
-       PointNode startPointNode = pointNodeDB.getPoint(startPointName);
-
-       // If the start point could not be found, throw an exception
-       if (startPointNode == null) {
-           throw new ParseException("Invalid segment: " + startPointName);
-       }
-
-       // Iterate over the array of end point names and create edges
-       for (String endPointName : endPointNames) {
-           PointNode endPointNode = pointNodeDB.getPoint(endPointName);
-
-           // If the end point could not be found, throw an exception
-           if (endPointNode == null) {
-               throw new ParseException("Invalid segment: " + endPointName);
-           }
-           segmentNodeDB.addUndirectedEdge(startPointNode, endPointNode);
-       }
-   }
-
-   public ComponentNode buildFigureNode(String description) {
-	   //build the figure
-       return figureNode = new FigureNode(description, pointNodeDB, segmentNodeDB);
-   }
-
-   public PointNodeDatabase getPointNodeDatabase() {
-       return pointNodeDB;
-   }
-
-   public SegmentNodeDatabase getSegmentNodeDatabase() {
-       return segmentNodeDB;
-   }
-
-   public FigureNode getFigureNode() {
-       return figureNode;
-   }
- 
+	public GeometryBuilder() { }
+	
+	public FigureNode buildFigureNode(String description, 
+										PointNodeDatabase points, 
+										SegmentNodeDatabase segments)
+	{
+		return new FigureNode(description, points, segments);
+	}
+	
+	
+	public SegmentNodeDatabase buildSegmentNodeDatabase() 
+	{
+		return new SegmentNodeDatabase();
+	}
+	
+	
+    public SegmentNode buildSegmentNode(PointNode pt1, PointNode pt2) 
+    {
+    	return new SegmentNode(pt1, pt2);
+    }
+    
+    
+    public PointNodeDatabase buildPointNodeDatabase(List<PointNode> points) 
+    {
+    	return new PointNodeDatabase();
+    }
+    
+    
+    public PointNode buildPointNode(String name, double x, double y) 
+    {
+    	return new PointNode(name, x, y);
+    }
 }
