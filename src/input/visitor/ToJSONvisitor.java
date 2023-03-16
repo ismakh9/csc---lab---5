5008cd67ee497273a0730fd74cb3b4eed60d4ef8
ismakh9 <122399506+ismakh9@users.noreply.github.com>
2023-03-16 10:54:59 -0400

    change
    changes

--- src/input/visitor/ToJSONvisitor.java
+++ src/input/visitor/ToJSONvisitor.java
@@ -7,106 +7,76 @@ import input.components.point.PointNode;
 import input.components.point.PointNodeDatabase;
 import input.components.segment.SegmentNode;
 import input.components.segment.SegmentNodeDatabase;
+import utilities.io.StringUtilities;
 
-public class ToJSONvisitor {
-	
-	public Object visitFigureNode(FigureNode node, Object o) {
-	    StringBuilder sb = (StringBuilder) o;
-	    
-	    sb.append("{\n");
-	    sb.append("\"type\": \"figure\",\n");
-	  
-	    sb.append("\"description\": \"");
-	    sb.append(node.getDescription());
-	    sb.append("\",\n");
-	    
-	    sb.append("\"pointDatabase\": ");
-	    sb.append(node.getPointsDatabase().accept((ComponentNodeVisitor) this, new StringBuilder()));
-	    sb.append(",\n");
-	            
-	    sb.append("\"segmentDatabase\": ");
-	    sb.append(node.getSegments().accept((ComponentNodeVisitor) this, new StringBuilder()));
-	    sb.append("\n");
-	    sb.append("}");
-	    
-	    return sb;
-	}
+import java.util.List;
 
-	public Object visitSegmentDatabaseNode(SegmentNodeDatabase node, Object o) {
-	    StringBuilder sb = (StringBuilder) o;
-	    
-	    sb.append("{\n");
-	    sb.append("\"type\": \"segmentDatabase\",\n");
-	    sb.append("\"segments\": [\n");
-	    List<SegmentNode> segments = node.getSegments();
-	    for (int i = 0; i < segments.size(); i++) {
-	        SegmentNode segmentNode = segments.get(i);
-	        sb.append(segmentNode.accept((ComponentNodeVisitor) this, new StringBuilder()));
-	        if (i < segments.size() - 1) {
-	            sb.append(",\n");
-	        }
-	    }
-	    sb.append("\n]\n");
-	    sb.append("}");
-	    return sb;
-	}
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-	public Object visitSegmentNode(SegmentNode node, Object o) {
-	    StringBuilder sb = (StringBuilder) o;
-	    
-	    sb.append("{\n");
-	    sb.append("\"type\": \"segment\",\n");
-	    sb.append("\"startPoint\": \"");
-	    sb.append(node.getPoint1().getName());
-	    sb.append("\",\n");
-	    sb.append("\"endPoint\": \"");
-	    sb.append(node.getPoint2().getName());
-	    sb.append("\"\n");
-	    sb.append("}");
-	    
-	    return sb;
-	}
+import input.components.FigureNode;
+import input.components.point.PointNode;
+import input.components.point.PointNodeDatabase;
+import input.components.segment.SegmentNode;
+import input.components.segment.SegmentNodeDatabase;
 
+public class ToJSONvisitor implements ComponentNodeVisitor {
 
-	public Object visitPointNode(PointNode node, Object o) {
-	    StringBuilder sb = (StringBuilder) o;
-	    
-	    sb.append("{\n");
-	    sb.append("\"type\": \"point\",\n");
-	    sb.append("\"name\": \"");
-	    sb.append(node.getName());
-	    sb.append("\",\n");
-	    sb.append("\"x\": ");
-	    sb.append(node.getX());
-	    sb.append(",\n");
-	    sb.append("\"y\": ");
-	    sb.append(node.getY());
-	    sb.append("\n");
-	    sb.append("}");
-	    
-	    return sb;
-	}
+    @Override
+    public Object visitFigureNode(FigureNode node, Object o) {
+        JSONObject jsonObject = new JSONObject();
+        //insert the all the tree stucture
+        jsonObject.put("type", "Figure");
+        jsonObject.put("description", node.getDescription());
+        jsonObject.put("pointDatabase", node.getPointsDatabase().accept(this, new JSONObject()));
+        jsonObject.put("segmentDatabase", node.getSegments().accept(this, new JSONArray()));
+        return jsonObject;
+    }
 
-	
-	public Object visitPointNodeDatabase(PointNodeDatabase node, Object o) {
-	    StringBuilder sb = (StringBuilder) o;
-	    
-	    sb.append("{\n");
-	    sb.append("\"type\": \"pointDatabase\",\n");
-	    sb.append("\"points\": [\n");
-	    List<PointNode> points = node.getPoints();
-	    for (int i = 0; i < points.size(); i++) {
-	        PointNode pointNode = points.get(i);
-	        sb.append(pointNode.accept((ComponentNodeVisitor) this, new StringBuilder()));
-	        if (i < points.size() - 1) {
-	            sb.append(",\n");
-	        }
-	    }
-	    sb.append("\n]\n");
-	    sb.append("}");
-	    
-	    return sb;
-	}
+    @Override
+    public Object visitSegmentDatabaseNode(SegmentNodeDatabase node, Object o) {
+        JSONArray jsonArray = new JSONArray();
+        //traverse through segmentdatabase and insert it in the jsonArray
+        List<SegmentNode> segments = node.getSegments();
+        for (SegmentNode segmentNode : segments) {
+            jsonArray.put(segmentNode.accept(this, new JSONObject()));
+        }
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "segmentDatabase");
+        //transfer the jsonArray to JsonObject
+        jsonObject.put("segments", jsonArray);
+        return jsonObject;
+    }
 
+    @Override
+    public Object visitSegmentNode(SegmentNode node, Object o) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "Segment");
+        jsonObject.put("startPoint", node.getPoint1().getName());
+        jsonObject.put("endPoint", node.getPoint2().getName());
+        return jsonObject;
+    }
 
+    @Override
+    public Object visitPointNode(PointNode node, Object o) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "Point");
+        jsonObject.put("name", node.getName());
+        jsonObject.put("x", node.getX());
+        jsonObject.put("y", node.getY());
+        return jsonObject;
+    }
+
+    @Override
+    public Object visitPointNodeDatabase(PointNodeDatabase node, Object o) {
+        JSONArray jsonArray = new JSONArray();
+        List<PointNode> points = node.getPoints();
+        for (PointNode pointNode : points) {
+            jsonArray.put(pointNode.accept(this, new JSONObject()));
+        }
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "pointDatabase");
+        jsonObject.put("points", jsonArray);
+        return jsonObject;
+    }
 }
